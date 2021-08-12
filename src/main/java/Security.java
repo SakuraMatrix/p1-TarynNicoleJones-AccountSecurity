d5249b88d4396995fc01fb6b609ce14b9f0e0d9c
megrad79 <megrad.79@gmail.com>
2021-08-12 16:58:03 -0400

    changed dependencies
    some dependecies cannot be found...

--- src/main/java/Security.java
+++
@@ -1,8 +0,0 @@
-
-
-public class Security{
-    public static void main(String [] args){
-        System.out.println("Taryn's World!");
-        // test
-    }
-}
---
+++ src/main/java/com/github/megrad79/AccountSecurity/Security.java
@@ -0,0 +1,20 @@
+package com.github.megrad79.AccountSecurity;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Security{
+    public static void main(String [] args) throws Exception {
+        System.out.println("Taryn's World!");
+
+        /*// File printout example
+        File f = new File("standup2.txt");
+        Scanner s = new Scanner(f);
+
+        while(s.hasNextLine()){
+            System.out.println(s.nextLine());
+        }*/
+
+    }
+}
