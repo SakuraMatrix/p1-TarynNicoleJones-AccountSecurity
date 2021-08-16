14b67db3ceb10af617adf577153b0c3e7773d8a2
megrad79 <megrad.79@gmail.com>
2021-08-15 21:18:38 -0400

    troubleshooting prototype according to instruction
    needs to be modified for unique project
    
    all working code!!!

--- src/main/java/com/github/megrad79/AccountSecurity/Security.java
+++ src/main/java/com/github/megrad79/AccountSecurity/Security.java
@@ -1,12 +1,17 @@
 package com.github.megrad79.AccountSecurity;
 
+import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Security{
-    public static void main(String [] args) throws Exception {
-        System.out.println("Taryn's World!");
+    public static void main(String [] args) {
+        //Testing Compile
+        // System.out.println("Taryn's World!");
 
         /*// File printout example
         File f = new File("standup2.txt");
@@ -15,6 +20,37 @@ public class Security{
         while(s.hasNextLine()){
             System.out.println(s.nextLine());
         }*/
+
+        DisposableServer server =
+        HttpServer.create()   // Prepares an HTTP server ready for configuration
+                //.host("localhost")
+                .port(8080)    // Configures the port number as 8080, this will let the system pick up
+                // an ephemeral port when binding the server
+
+                // Routing HTTP per Section 5.3 example at
+                // https://projectreactor.io/docs/netty/release/reference/index.html#_routing_http
+                .route(routes ->
+                        routes.get("/hello",
+                                        (request, response) -> response.sendString(Mono.just("Hello World!")))
+                                .post("/echo",
+                                        (request, response) -> response.send(request.receive().retain()))
+                                .get("/path/{param}",
+                                        (request, response) -> response.sendString(Mono.just(request.param("param"))))
+                                .ws("/ws",
+                                        (wsInbound, wsOutbound) -> wsOutbound.send(wsInbound.receive().retain())))
+                .bindNow();
+
+
+                       /* // The server will respond only on POST requests
+                        // where the path starts with /test and then there is path parameter
+                        routes.post("/test/{param}", (request, response) ->
+                                response.sendString(request.receive()
+                                        .asString()
+                                        .map(s -> s + ' ' + request.param("param") + '!')
+                                        .log("http-server"))))
+                .bindNow(); // Starts the server in a blocking fashion, and waits for it to finish its initialization
+*/
+        server.onDispose().block();
 
     }
 }
---
+++ src/test/java/SecurityTest.java
@@ -0,0 +1,15 @@
+import com.github.megrad79.AccountSecurity.Security;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class SecurityTest {
+
+    @Test
+    // Check reading outputs
+    public void UnitTest(){
+        // Test Print Inputs
+        assertTrue(true);
+    }
+
+}
