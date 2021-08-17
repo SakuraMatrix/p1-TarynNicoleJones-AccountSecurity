7a7351eb982043f8352ca804f3b27e40be79347f
megrad79 <megrad.79@gmail.com>
2021-08-17 15:59:31 -0400

    added path to error.html as in demo
    still need to customise program; might refer to Tam's project for inspiration

--- src/main/java/com/github/megrad79/AccountSecurity/Security.java
+++ src/main/java/com/github/megrad79/AccountSecurity/Security.java
@@ -6,10 +6,13 @@ import reactor.netty.http.server.HttpServer;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Security{
-    public static void main(String [] args) {
+    public static void main(String [] args) throws URISyntaxException {
         //Testing Compile
         // System.out.println("Taryn's World!");
 
@@ -20,24 +23,28 @@ public class Security{
         while(s.hasNextLine()){
             System.out.println(s.nextLine());
         }*/
+        Path file = Paths.get(Security.class.getResource("/error.html").toURI());
 
         DisposableServer server =
-        HttpServer.create()   // Prepares an HTTP server ready for configuration
-                //.host("localhost")
-                .port(8080)    // Configures the port number as 8080, this will let the system pick up
-                // an ephemeral port when binding the server
+            HttpServer.create()   // Prepares an HTTP server ready for configuration
+            //.host("localhost")
+            .port(8080)    // Configures the port number as 8080, this will let the system pick up
+            // an ephemeral port when binding the server
 
-                // Routing HTTP per Section 5.3 example at
-                // https://projectreactor.io/docs/netty/release/reference/index.html#_routing_http
-                .route(routes ->
-                        routes.get("/hello",
-                                        (request, response) -> response.sendString(Mono.just("Hello World!").log("http-server")))
-                                .post("/echo",
-                                        (request, response) -> response.send(request.receive().retain().log("http-server")))
-                                .get("/path/{param}",
-                                        (request, response) -> response.sendString(Mono.just(request.param("param")).log("http-server")))
-                                .ws("/ws",
-                                        (wsInbound, wsOutbound) -> wsOutbound.send(wsInbound.receive().retain().log("http-server"))))
+            // Routing HTTP per Section 5.3 example at
+            // https://projectreactor.io/docs/netty/release/reference/index.html#_routing_http
+            .route(routes ->
+                routes.get("/hello",
+                            (request, response) -> response.sendString(Mono.just("Hello World!").log("http-server")))
+                    .post("/echo",
+                            (request, response) -> response.send(request.receive().retain().log("http-server")))
+                    .get("/path/{param}",
+                            (request, response) -> response.sendString(Mono.just(request.param("param")).log("http-server")))
+                    .ws("/ws",
+                            (wsInbound, wsOutbound) -> wsOutbound.send(wsInbound.receive().retain().log("http-server")))
+                    .get("/test",
+                            (request, response) -> response.status(404).addHeader("Message","Warning - missing content").sendFile(file))
+                )
                 .bindNow();
 
 
